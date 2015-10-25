a777f63b984cb897266b5a51fa502498bd093576
gogoout <gogoout@gogoouts-MacBook-Pro.local>
2015-10-25 14:26:25 +0800

    first release , auto detect the rtos class

--- library.js
+++ library.js
@@ -1,7 +1,6 @@
 "use strict";
 
 
-
 var controllers = require('./lib/controllers'),
 	winston = module.parent.require('winston'),
 	User = module.parent.require('./user'),
@@ -13,91 +12,101 @@ var controllers = require('./lib/controllers'),
 	async = module.parent.require('async'),
 	_ = module.parent.require('underscore'),
 	SocketPlugins = module.parent.require('./socket.io/plugins'),
+	cheerio = require('cheerio'),
 
 	plugin = {};
 
-plugin.init = function(params,callback){
+function replyToSeeFilter(uid, post, callback) {
+	Topics.getTopicField(parseInt(post.tid, 10), 'replyerIds', function (err, replyerIds) {
+		var isRtos = /<p class="rtos">/.test(post.content);
+		if (isRtos) {
+			winston.info('[RtoS] get rtos post, tid : ' + post.tid);
+			var match = false;
+			User.isAdministrator(uid, function (err, isAdmin) {
+				if (isAdmin || uid == post.uid) {
+					match = true;
+				}
+				else {
+					if (replyerIds) {
+						if (_.indexOf(JSON.parse(replyerIds), parseInt(uid)) >= 0) {
+							match = true;
+						}
+					}
+				}
+
+				winston.info('[RtoS] match: ' + match);
+				if (!match) {
+//					Meta.settings.get('reply2see', function (err, settings) {
+//					});
+//					winston.info('[RtoS] content before:\n' + post.content);
+					var $ = cheerio.load(post.content);
+					$('p.rtos').each(function (idx, element) {
+						$(element).replaceWith($('<code class="rtos">[内容回复后并刷新后可见！]</code>'));
+					});
+					post.content = $.html();
+//					winston.info('[RtoS] content after:\n' + post.content);
+					callback(null, post);
+				}
+				else {
+					callback(null, post);
+				}
+			})
+		}
+		else {
+			callback(null, post);
+		}
+	})
+}
+
+
+plugin.init = function (params, callback) {
 	var router = params.router,
 		hostMiddleware = params.middleware,
 		hostControllers = params.controllers;
-		
+
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
 
 	router.get('/admin/plugins/reply2see', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
 	router.get('/api/admin/plugins/reply2see', controllers.renderAdminPage);
 
-	handleSocketIO();
 	callback();
-}
-
-
-plugin.getPostContent = function(data, callback) {	
-	var match = true;
-	var post;
-	
-	Topics.getTopicData(parseInt(data.posts[0].tid,10),function(err,fields){
-		if (fields.rtos) {
-			match = false;
-			User.isAdministrator(data.uid,function(err,isAdmin){
-				if (isAdmin || data.uid == data.posts[0].uid) {
-					match = true;
-				} else {
-					if (fields.replyerIds) {
-						 if (_.indexOf(JSON.parse(fields.replyerIds),parseInt(data.uid)) >= 0 ) {
-						 	match = true;
-						 }
-					}
-				}
-				if (!match) {
-					Meta.settings.get('reply2see', function (err, settings) {
-						if (fields.title.match(settings.title)) {
-							data.posts[0].content=data.posts[0].content.replace(/<p class="rtos">[\s\S\n]*<\/p>/gm,'<code>[内容回复后并刷新后可见！]</code>')
-						} else {
-							data.posts[0].content = "<code>[内容回复后并刷新后可见！]</code>";
-						}
-						callback (null,data);
-					});
-				} else {
-				callback (null,data);
-				}
-			})
-		} else {
-			callback(null,data);
-		}
-	})
 };
 
-plugin.setReplyerId = function(data,callback) {
-	 Topics.getTopicData(parseInt(data.tid),function(err,fields){
-	 	if (fields.rtos) { 
+plugin.getPostContent = function (data, callback) {
+	replyToSeeFilter(data.uid, data.posts[0], function (err, post) {
+		callback(err, data);
+	});
+};
 
-				var replyerIds = fields.replyerIds?JSON.parse(fields.replyerIds):[];
-				if ( _.indexOf(replyerIds,parseInt(data.uid)) === -1) {
-					replyerIds.push(parseInt(data.uid));
-					Topics.setTopicField(parseInt(data.tid),'replyerIds',JSON.stringify(replyerIds));
-				}
-		} 
-    })
-	callback(null,data);
-}
+plugin.setReplyerId = function (data, callback) {
+	Topics.getTopicData(parseInt(data.tid), function (err, fields) {
 
-plugin.addAdminNavigation = function(header, callback) {
+		var replyerIds = fields.replyerIds ? JSON.parse(fields.replyerIds) : [];
+		if (_.indexOf(replyerIds, parseInt(data.uid)) === -1) {
+			replyerIds.push(parseInt(data.uid));
+			Topics.setTopicField(parseInt(data.tid), 'replyerIds', JSON.stringify(replyerIds));
+		}
+	});
+	callback(null, data);
+};
+
+plugin.addAdminNavigation = function (header, callback) {
 	header.plugins.push({
 		route: '/plugins/reply2see',
-		icon: 'fa-tint',
-		name: 'Rely post to see content'
+		icon : 'fa-tint',
+		name : 'Rely post to see content'
 	});
 
 	callback(null, header);
 };
 
-function handleSocketIO() {
-	SocketPlugins.RtoS = {};
-	SocketPlugins.RtoS.setRtoS = function(socket, data, callback) {		
-		Topics.setTopicField(data.tid,'rtos',1);
-		callback;	
-	};
+plugin.getPostSummaryByPids = function (data, callback) {
+	async.each(data.posts, function (post, aCallback) {
+		replyToSeeFilter(data.uid, post, aCallback);
+	}, function (err) {
+		callback(err, data);
+	});
+};
 
-}
 module.exports = plugin;
--- static/templates/admin/plugins/reply2see.tpl
+++ static/templates/admin/plugins/reply2see.tpl
@@ -5,11 +5,8 @@
 			<div class="panel-body">
 				<form role="form" class="reply2see">
 					<div class="form-group">
-						<label for="title">设置部分隐藏模版</label>
-						<p>如果这个帖子设置为回复才可见，需要制定标题模版和内容模版</p>
-						<p>标题模版包括的内容，例如<code>XXXX回复可见贴</code> 如果标题不符合，全部内容都会隐藏。</p>
-						<input type="text" id="title" name="title" title="Title" class="form-control" placeholder="回复可见"><br />
-						<p>内容模版中符合相关内容将会被隐藏，缺省设置为<code>&ltp class="rtos"&gtXXXXX&lt\p&gt</code> 其中的XXXXX内容将会被隐藏，回复后才可见。这个不需要额外设置。</p>
+						<label for="title">目前无设置</label>
+						<p>内容模版中符合相关内容将会被隐藏，缺省设置为<code>&lt;p class="rtos"&gt;XXXXX&lt;/p&gt;</code> 其中的XXXXX内容将会被隐藏，回复后才可见。这个不需要额外设置。</p>
 					</div>
 				</form>
 			</div>
